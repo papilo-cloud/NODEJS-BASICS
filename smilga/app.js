61439ca727d23a0c30e77adc38078a19967cbb6d
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-06-18 13:23:01 +0100

    fixed error

--- smilga/app.js
+++ smilga/app.js
@@ -1,18 +1,9 @@
 const http = require('http')
 
 const server =http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Welcome to the homepage')
-    }
-    if (req.url === '/about') {
-        res.end('Thos is the about page')
-    }
-    res.end(
-        `
-        <h1>OOPS</h1>
-        <p>This page you're looking for doesn't exist... keep browsing.</p>
-        `
-    )
+    res.write('Hello Node.js')
+    res.end()
 
 })
 server.listen(8000)
+console.log('Hello people')
