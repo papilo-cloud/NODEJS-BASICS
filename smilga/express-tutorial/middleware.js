e1253a9846acca5642ac284402626c7eb839c4a5
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-06-24 16:41:00 +0100

    adding the logger function as an external module

--- smilga/express-tutorial/middleware.js
+++ smilga/express-tutorial/middleware.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const app = express()
+const {logger} = require('./logger')
 
 // req => middleware => res 
 
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const year = new Date().getFullYear();
-    console.log(method, url, year)
-    // res.send('Listening')
-    next()
-}
+// We are importing the middleware as a separate module.
+// const logger = (req, res, next) => {
+//     const method = req.method;
+//     const url = req.url;
+//     const year = new Date().getFullYear();
+//     console.log(method, url, year)
+//     // res.send('Listening')
+//     next()
+// }
 
-app.get('/', (req, res) => {
+app.get('/', logger, (req, res) => {
     res.send('Home')
 })
-app.get('/about', (req, res) => {
+app.get('/about', logger, (req, res) => {
     res.send('About')
 })
 
-app.listen(8000, () => {
+app.listen(5000, () => {
     'App listening on port 8000'
 })
