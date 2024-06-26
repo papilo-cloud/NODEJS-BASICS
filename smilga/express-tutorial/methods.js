b95d38a93c8569875ff62f5c76b6b694ef8afead
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-06-26 11:03:20 +0100

    done with basic HTTP methods

--- smilga/express-tutorial/methods.js
+++ smilga/express-tutorial/methods.js
@@ -8,6 +8,15 @@ app.use(express.urlencoded({extended: false}))
 // parse json 
 app.use(express.json())
 
+app.post('/login', (req, res) => {
+    const {name} = req.body;
+
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    } else {
+        return res.status(401).send('Please provide credentials')
+    }
+})
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people})
@@ -22,14 +31,60 @@ app.post('/api/people', (req, res) => {
     }
     res.status(201).json({success: true, person: name})
 })
-app.post('/login', (req, res) => {
-    const {name} = req.body;
 
-    if (name) {
-        return res.status(200).send(`Welcome ${name}`)
-    } else {
-        return res.status(401).send('Please provide credentials')
+app.post('/api/postman/people', (req, res) => {
+    const { name, id } = req.body;
+    if (!name) {
+        return res
+            .status(400)
+            .json({success: false, msg: 'please provide a name value'})
     }
+    res.status(201).json({success: true, data: [...people, {id, name}]})
+})
+
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    const person = people.find(person => person.id === Number(id))
+    if (!person) {
+        return res
+                .status(404)
+                .json(
+                        {
+                            success: false, 
+                            msg: `no person with id ${id} found`
+                        }
+                    )
+    }
+    const newPeople = people.map(person => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(201)
+        .json(newPeople)
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const {id} = req.params
+
+    const person = people.find(person => person.id === Number(id))
+    if (!person) {
+        return res
+                .status(404)
+                .json(
+                    {
+                        success: false, 
+                        msg: `no person with id ${id} found`
+                    }
+                )
+    }
+    const newPeople = people.filter(person => person.id !== Number(id))
+    res.status(201)
+        .json(newPeople)
 })
 
 app.listen(5000, () => {
