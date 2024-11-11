92fc9802d32464be6c4900e513575b09ef88848c
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-11 01:27:59 +0100

    index

--- Abdul/pony-express/index.js
+++ Abdul/pony-express/index.js
@@ -3,10 +3,17 @@ const app = express()
 const usersRouter = require('./routes/users')
 const emailsRouter = require('./routes/emails')
 const logger = require('./lib/logger')
+const compress = require('compression')
+const path = require('path')
 
 
+app.use(express.json())
 app.use(logger)
+app.use(compress())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use('/users', usersRouter)
 app.use('/emails', emailsRouter)
+
 
 app.listen(3000)
