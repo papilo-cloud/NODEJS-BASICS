00a8a0f75d2bdbde105324cb845c1e4dd8d3ab17
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-11 01:25:09 +0100

    email route

--- Abdul/pony-express/routes/emails.js
+++ Abdul/pony-express/routes/emails.js
@@ -1,21 +1,44 @@
 const express = require('express')
+const path = require('path')
 const emails = require('../fixtures/emails.json')
-const readBody = require('../lib/read-body')
 const generateId = require('../lib/generate-id')
+const multer = require('multer')
 const emailsRouter = express.Router()
 
-let getEmailsRoute = (req, res) => {
+
+// let upload = multer({
+//     dest: path.join(__dirname, '../uploads')
+// })
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, res, cb) => {
+        cb(null, 'hello.jpg')
+    }
+})
+class NotFound extends Error {
+    constructor(message) {
+        super(message)
+        this.name = 'NotFound'
+    }
+}
+let getEmailsRoute = (req, res, next) => {
     res.send(emails)
 }
 
 let getEmailRoute = (req, res) => {
     let email = emails.find(email => email.id == req.params.id)
+    if (!email) {
+        throw new NotFound();
+    }
     res.send(email)
 }
 
 let createEmailRoute = async (req, res) => {
-    let body = await readBody(req)
-    let newEmail = {...JSON.parse(body), id: generateId()}
+    let file = (req.files || []).map(file => file.filename)
+    let newEmail = {...req.body, id: generateId(), file}
     emails.push(newEmail)
     res.status(201)
     res.send(newEmail)
@@ -24,9 +47,8 @@ let createEmailRoute = async (req, res) => {
 // Mutation is generally a bad practice, but we just used mutation to keep
 // things simple. Production backends would typically use a DB anyways
 let updateEmailRoute = async (req, res) => {
-    let body = await readBody(req)
     let email = emails.find(email => email.id == req.params.id)
-    Object.assign(email, JSON.parse(body)) 
+    Object.assign(email, req.body) 
     res.status(200)
     res.send(email)
 }
@@ -37,9 +59,11 @@ let deleteEmailRoute = (req, res) => {
     res.sendStatus(204)
 }
 
+const upload = multer({storage})
+
 emailsRouter.route('/')
     .get(getEmailsRoute)
-    .post(createEmailRoute)
+    .post(upload.array('file'), createEmailRoute)
 emailsRouter.route('/:id')
     .get(getEmailRoute)
     .put(updateEmailRoute)
